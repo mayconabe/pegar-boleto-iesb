3fb4ef9fbc83000040fc833a1bf3f977fab66887
Maycon <maycon.abe01@gmail.com>
2024-03-26 10:16:48 -0300

    v1.0

--- pegar_boleto_iesb.py
+++ pegar_boleto_iesb.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 # Inicializa o WebDriver
 options = webdriver.ChromeOptions()
@@ -33,10 +34,21 @@ WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/ht
 
 # Aperta no botão "Boletos"
 driver.find_element(By.XPATH,"/html/body/ion-nav-view/ion-side-menus/div/ion-side-menu/menu-left/ion-content/div[1]/ion-list/div/div[4]/div/ion-item[6]/div[2]/a").click()
-
+time.sleep(50)
 # Verifica se há boletos. Se não houver, retorna uma mensagem de erro.
-# try:
+
+WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/ion-nav-view/ion-side-menus/div/ion-side-menu-content/ion-nav-view/ion-view/ion-content/div[1]/div/boleto-deck/div/div/div[2]/boleto-detail/div/div/div/a[2]"))).click()
+
+time.sleep(2)
+
+if driver.find_element(By.XPATH, "/html/body/div[5]/div[2]/ion-modal-view/ion-content/div/div[1]/p").text == "Carregando código de barras":
+    time.sleep(10)
+    boleto = driver.find_element(By.XPATH, "/html/body/div[5]/div[2]/ion-modal-view/ion-content/div/div[1]/p").text
+    print(boleto)
+else:
+    boleto = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[5]/div[2]/ion-modal-view/ion-content/div/div[1]/p"))).text
+    print(boleto)
 
 
 # Fecha o navegador
-# driver.quit()
+driver.quit()
