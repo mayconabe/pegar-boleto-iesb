5269a67d4fe74ca63359a5cf028ccbb857cf8e52
mayconabe <maycon.abe01@gmail.com>
2024-04-03 11:22:23 -0300

    v1.1

--- pegar_boleto_iesb.py
+++ pegar_boleto_iesb.py
@@ -4,19 +4,24 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 # Inicializa o WebDriver
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+options.add_argument('headless')
+options.add_argument('log-level=3')
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(1920, 1080, driver.window_handles[0])
 
 # Abre a página desejada
 driver.get('https://aluno.iesb.br/aluno/#/login')
 
-matricula = input("Matricula: ")
-senha = input("Senha: ")
+# matricula = input("Matricula: ")
+# senha = input("Senha: ")
+matricula = ""
+senha = ""
 
 # Encontra os elementos de Login(Username e Password)
 username = driver.find_element(By.XPATH, "/html/body/ion-nav-view/div/div/div[2]/div/form/div/label[1]/input")
@@ -34,14 +39,14 @@ WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/ht
 
 # Aperta no botão "Boletos"
 driver.find_element(By.XPATH,"/html/body/ion-nav-view/ion-side-menus/div/ion-side-menu/menu-left/ion-content/div[1]/ion-list/div/div[4]/div/ion-item[6]/div[2]/a").click()
-time.sleep(50)
+time.sleep(35)
 # Verifica se há boletos. Se não houver, retorna uma mensagem de erro.
 
 WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/ion-nav-view/ion-side-menus/div/ion-side-menu-content/ion-nav-view/ion-view/ion-content/div[1]/div/boleto-deck/div/div/div[2]/boleto-detail/div/div/div/a[2]"))).click()
 
 time.sleep(2)
 
-if driver.find_element(By.XPATH, "/html/body/div[5]/div[2]/ion-modal-view/ion-content/div/div[1]/p").text == "Carregando código de barras":
+if driver.find_element(By.XPATH, "/html/body/div[5]/div[2]/ion-modal-view/ion-content/div/div[1]/p").text == "Carregando código de barras" or driver.find_element(By.XPATH, "/html/body/div[5]/div[2]/ion-modal-view/ion-content/div/div[1]/p").text == "loading bar code":
     time.sleep(10)
     boleto = driver.find_element(By.XPATH, "/html/body/div[5]/div[2]/ion-modal-view/ion-content/div/div[1]/p").text
     print(boleto)
